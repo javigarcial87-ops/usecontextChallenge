b44c2500a734adc397d2648b3cfc25548406c7dd
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-11 17:57:17 +0100

    pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useContext } from 'react';
+import { ThemeProvider, ThemeContext } from './themes/ThemeContext';
+import RoutesApp from './routes/RoutesApp';
 import './App.css'; // Archivo de estilos CSS
 
-const App = () => {
-  return (
-<></>
-  );
-};
+
+function AppContent() {
+  const {theme} = useContext(ThemeContext)
+
+  return(
+    <>
+    <section className={`App${theme}`}>
+    <RoutesApp/>
+    </section>
+    </>
+  )
+}
+
+
+
+function App() {
+  return(
+      <ThemeProvider>
+        <AppContent/>
+      </ThemeProvider>
+  )
+} 
 
 export default App;
